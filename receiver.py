af3a7e1eda2de7a8e1d23283837fe07f3a5e411f
oscarteeninga <oscarteeninga@gmail.com>
2020-05-04 00:10:39 +0200

    Remove Darknet

--- receiver.py
+++ receiver.py
@@ -1,62 +1,108 @@
 import cv2
 import numpy as np
-import time
-from darkflow.net.build import TFNet
-import _thread
 
-
-options = {"model": "cfg/yolo.cfg", 
-           "load": "bin/yolo.weights", 
-           "threshold": 0.1, 
-           "gpu": 1.0}
-
-tfnet = TFNet(options)
-
-def boxing(original_img, predictions):
-    newImage = np.copy(original_img)
-
-    for result in predictions:
-        top_x = result['topleft']['x']
-        top_y = result['topleft']['y']
-
-        btm_x = result['bottomright']['x']
-        btm_y = result['bottomright']['y']
-
-        confidence = result['confidence']
-        label = result['label'] + " " + str(round(confidence, 3))
-
-        if confidence > 0.3:
-            newImage = cv2.rectangle(newImage, (top_x, top_y), (btm_x, btm_y), (255,0,0), 3)
-            newImage = cv2.putText(newImage, label, (top_x, top_y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL , 0.8, (0, 230, 0), 1, cv2.LINE_AA)
-            
-    return newImage
-
-step = 0
 capture = cv2.VideoCapture('rtsp://admin:10S7r13pe93@192.168.1.105:554')
 
-results = 0
+# 'path to input image/video'
+IMAGE='./1.mp4'
 
-def predict(frame):
-    global results
-    stime = time.time()
-    results = tfnet.return_predict(frame)
-    print(time.time() - stime)
+# 'path to yolo config file'
+# download https://github.com/arunponnusamy/object-detection-opencv/blob/master/yolov3.cfg
+CONFIG='cfg/yolo.cfg'
+
+# 'path to text file containing class names'
+# download https://github.com/arunponnusamy/object-detection-opencv/blob/master/yolov3.txt
+CLASSES='./yolov3.txt'
+
+# 'path to yolo pre-trained weights'
+# wget https://pjreddie.com/media/files/yolov3.weights
+WEIGHTS='./bin/yolo.weights'
+
+classes = None
+with open(CLASSES, 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+
+scale = 0.00392
+conf_threshold = 0.5
+nms_threshold = 0.4
+
+# generate different colors for different classes
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+def get_output_layers(net):
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    return output_layers
+
+# function to draw bounding box on the detected object with class name
+def draw_bounding_box(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    label = str(classes[class_id])
+    color = COLORS[class_id]
+    cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
+    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+def processImage(image, index):
+    Width = image.shape[1]
+    Height = image.shape[0]
+
+    # read pre-trained model and config file
+    net = cv2.dnn.readNet(WEIGHTS, CONFIG)
+
+    # create input blob
+    blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+    # set input blob for the network
+    net.setInput(blob)
+
+    # run inference through the network
+    # and gather predictions from output layers
+    outs = net.forward(get_output_layers(net))
+
+    # initialization
+    class_ids = []
+    confidences = []
+    boxes = []
+    # for each detetion from each output layer
+    # get the confidence, class id, bounding box params
+    # and ignore weak detections (confidence < 0.5)
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+    # apply non-max suppression
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    # go through the detections remaining
+    # after nms and draw bounding box
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+
+        draw_bounding_box(image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
+
+    # display output image
+    out_image_name = "object detection"
+    cv2.imshow(out_image_name, image)
 
 while(True):
     ret, frame = capture.read()
     if ret == True:
-        frame = np.asarray(frame)
-        if step == 0:
-            _thread.start_new_thread(predict, (frame, ))
-            step = 40
-        else:
-            step -= 1
-
-        if results:
-            new_frame = boxing(frame, results)
-        else:
-            new_frame = frame
-        cv2.imshow('frame',new_frame)
+        processImage(frame, 1)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
