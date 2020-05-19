396cfef915ae2213229f5d4f12a170c874f5a415
Tomasz Kolbusz <tomaszkolbusz1@gmail.com>
2020-05-19 14:39:33 +0200

    Saving data with sqlite

--- repository.py
+++ repository.py
@@ -5,18 +5,19 @@ conn = sqlite3.connect("data.db")
 c = conn.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS events
-             (date datetime, object varchar(255))''')
+             (date datetime, confidence varchar(50), object varchar(50))''')
 
 conn.commit()
 
 
-def insert(objectname):
+def insert(objectname, confidence):
     c = conn.cursor()
-    c.execute("INSERT INTO events(date,object) VALUES (current_timestamp ,?)", [objectname])
+    c.execute("INSERT INTO events(date,object,confidence) VALUES (current_timestamp ,?,?)",
+              [objectname, confidence])
     conn.commit()
 
 
 def read():
     cur = conn.cursor()
-    cur.execute("select object,date from events")
+    cur.execute("select object,date,confidence from events")
     return cur.fetchall()
